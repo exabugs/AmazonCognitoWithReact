f4c39358e5f7dd32241c8b1d6fc0363ff674e4b7
Fujita Yutaro <a302012040@gmail.com>
2019-09-08 14:01:05 +0900

    second commit

---
+++ src/awsConfiguration.ts
@@ -0,0 +1,8 @@
+const awsConfiguration = {
+  region: 'ap-northeast-1',
+  IdentityPoolId: 'ap-northeast-1:####################',
+  UserPoolId: 'ap-northeast-1_####################',
+  ClientId: '####################',
+}
+
+export default awsConfiguration
